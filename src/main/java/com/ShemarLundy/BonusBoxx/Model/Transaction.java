04fe294adc0b61ecc702d430ada9443e2d009f3a
shemarlundy <shemarlund@gmail.com>
2023-03-07 22:57:49 -0500

    new dev branch with all the models and tables created with proper relationships

--- src/main/java/com/ShemarLundy/BonusBoxx/Model/Transaction.java
+++ src/main/java/com/ShemarLundy/BonusBoxx/Model/Transaction.java
@@ -27,4 +27,83 @@ public class Transaction {
     @JoinColumn(name = "store_id")
     private Store store;
 
+    public Long getID() {
+        return ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    public String getAmount_spent() {
+        return amount_spent;
+    }
+
+    public void setAmount_spent(String amount_spent) {
+        this.amount_spent = amount_spent;
+    }
+
+    public String getReciept_number() {
+        return reciept_number;
+    }
+
+    public void setReciept_number(String reciept_number) {
+        this.reciept_number = reciept_number;
+    }
+
+    public LocalDateTime getIssue_date() {
+        return issue_date;
+    }
+
+    public void setIssue_date(LocalDateTime issue_date) {
+        this.issue_date = issue_date;
+    }
+
+    public LocalDateTime getExpiration_date() {
+        return expiration_date;
+    }
+
+    public void setExpiration_date(LocalDateTime expiration_date) {
+        this.expiration_date = expiration_date;
+    }
+
+    public Boolean getWas_bought() {
+        return was_bought;
+    }
+
+    public void setWas_bought(Boolean was_bought) {
+        this.was_bought = was_bought;
+    }
+
+    public Reward getReward() {
+        return reward;
+    }
+
+    public void setReward(Reward reward) {
+        this.reward = reward;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public Store getStore() {
+        return store;
+    }
+
+    public void setStore(Store store) {
+        this.store = store;
+    }
 }
