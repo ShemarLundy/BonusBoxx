68a413cdbc27ed3a1f048f7438efd65516187aa4
shemarlundy <shemarlund@gmail.com>
2023-03-07 22:54:05 -0500

    new dev branch with all the models and tables created with proper relationships

---
+++ src/main/java/com/ShemarLundy/BonusBoxx/BonusBoxxApplication.java
@@ -0,0 +1,13 @@
+package com.ShemarLundy.BonusBoxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonusBoxxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonusBoxxApplication.class, args);
+	}
+
+}
